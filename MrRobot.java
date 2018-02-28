81152f034076d12245fbdb9df0971391132e9185
Jay C <35088530+jaykafkachen@users.noreply.github.com>
2018-02-28 12:11:34 -0800

    Update MrRobot.java
    Very bare bones structure I'm thinking of for movement/enemy info storage. Based on geometrical points on field.

--- MrRobot.java
+++ MrRobot.java
@@ -1,56 +1,88 @@
 package TeamMrRobot;
 import robocode.*;
-//import java.awt.Color;
+import robocode.util.Utils.*;
+import java.util.*;
+import java.awt.geom.*;
+import java.awt.*;
 
-// API help : http://robocode.sourceforge.net/docs/robocode/robocode/Robot.html
-
-/**
- * MrRobot - a robot by Jay Chen and Tyler Stamp 
+/*
+ * This code uses code from Shiz by Kawigi, which is released under the terms of the KPL.
  */
-public class MrRobot extends Robot
+public class MrRobot extends AdvancedRobot
 {
-	/**
-	 * run: Mrrobot's default behavior
-	 */
-	public void run() {
-		// Initialization of the robot should be put here
-
-		// After trying out your robot, try uncommenting the import at the top,
-		// and the next line:
-
-		// setColors(Color.red,Color.blue,Color.green); // body,gun,radar
-
-		// Robot main loop
-		while(true) {
-			// Replace the next 4 lines with any behavior you would like
-			ahead(100);
-			turnGunRight(360);
-			back(100);
-			turnGunRight(360);
+	static Point2D location, last;
+	static String target;
+	static Hashtable enemies;
+	static final MAXRADS = Math.PI*2;
+	
+	public void run()
+	{
+		setTurnRadarRight(Double.POSITIVE_INFINITY);
+		setColors(Color.black, Color.red, Color.red);
+		//MAXRADS = Math.PI*2;
+		enemies = new Hashtable(); //will store Key String enemy name, Value Point2d enemy location
+		target = "";
+		Point2D next = null;
+		
+		do
+		{
+			location = new Point2D.Double(getX(), getY());
+			
+				
+				do
+				{
+					angle += .1;
+				} while (angle < MAXRADS);
+			execute();
 		}
-	}
-
-	/**
-	 * onScannedRobot: What to do when you see another robot
-	 */
-	public void onScannedRobot(ScannedRobotEvent e) {
-		// Replace the next line with any behavior you would like
-		fire(1);
-	}
-
-	/**
-	 * onHitByBullet: What to do when you're hit by a bullet
-	 */
-	public void onHitByBullet(HitByBulletEvent e) {
-		// Replace the next line with any behavior you would like
-		back(10);
+		while (true);
 	}
 	
-	/**
-	 * onHitWall: What to do when you hit a wall
-	 */
-	public void onHitWall(HitWallEvent e) {
-		// Replace the next line with any behavior you would like
-		back(20);
-	}	
+	
+	public void onScannedRobot(ScannedRobotEvent e)
+	{
+		
+	}
+	
+	public void onHitByBullet(HitByBulletEvent e)
+	{
+		
+	}
+	
+	public void onRobotDeath(RobotDeathEvent e)
+	{
+		if (target.equals(e.getName()))
+		{
+			enemies.remove(e.getName());
+			target = "";
+		}
+	}
+	
+	//METHOD TO CALC ABSBEARING BETWEEN 2 POINTS
+	public static double absBearing(double x1, double y1, double x2, double y2) 
+	{
+  	double xAbs = x2-x1;
+  	double yAbs = y2-y1;
+  	double hyp = Point2D.distance(x1, y1, x2, y2);
+  	double arcSin = Math.asin(xAbs / hyp);
+  	double bearing = 0;
+  
+		if (xAbs > 0 && yAbs > 0) 
+		{ 
+			bearing = arcSin;
+		}
+		else if (xAbs < 0 && yAbs > 0) 
+		{ 
+			bearing = MAXRADS + arcSin; 
+		}
+		else if (xAbs > 0 && yAbs < 0) 
+		{ 
+			bearing = Math.PI - arcSin;
+		}
+		else if (xAbs < 0 && yAbs < 0) 
+		{ 
+			bearing = Math.PI + arcSin; 
+		}
+		return bearing;
+  	}
 }
