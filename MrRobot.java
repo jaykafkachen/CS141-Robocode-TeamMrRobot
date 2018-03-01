4ad641469b75beb0bec2d171befb018f906edec7
Jay C <35088530+jaykafkachen@users.noreply.github.com>
2018-02-28 21:40:26 -0800

    Update MrRobot.java
    More barebones stuff trig methods figured out now to focus on risk calculation.

--- MrRobot.java
+++ MrRobot.java
@@ -1,21 +1,21 @@
-package TeamMrRobot;
+package testing;
 import robocode.*;
-import robocode.util.Utils.*;
+import robocode.util.*;
 import java.util.*;
 import java.awt.geom.*;
 import java.awt.*;
 
-public class MrRobot extends AdvancedRobot
+public class Testbot extends AdvancedRobot
 {
 	static Point2D location, last;
-	static Hashtable enemies;
+	static Hashtable<String, Point2D> enemies;
 	static final double MAXRADS = Math.PI*2;
 	
 	public void run()
 	{
 		setTurnRadarRight(Double.POSITIVE_INFINITY);
 		setColors(Color.black, Color.red, Color.red);
-		enemies = new Hashtable(); //will store Key String enemy name, Value Point2d enemy location		
+		enemies = new Hashtable<String, Point2D>(); //will store Key String enemy name, Value Point2d enemy location		
 		Point2D next = null;
 
 		do
@@ -25,44 +25,47 @@ public class MrRobot extends AdvancedRobot
 				last = location;
 			boolean changed = false;
 			double moveAngle, distance;
-			angle = distance = 0.0;
+			moveAngle = distance = 0.0;
 			do
 			{
-				if ()
-				{
-					changed = true;
-					next = p; //next equals calculated risk next point
-				}
+			//	if ()//find the risk of different points, if there's a good new point, then move there
+			//	{
+			//		changed = true;
+			//		next = p; //next equals calculated risk next point
+			//	}
 				moveAngle += .1;
 			} while(moveAngle < MAXRADS);
 			if (changed)
 				last = location;
-			double turn = angle(next, location)-getHeadingRadians();
+			double turn = angle(next, location)-getHeadingRadians(); //nullPointer here, will be fixed when risk() method written
 			if (Math.cos(turn) < 0)
 			{
 				turn += Math.PI;
 				distance = -distance;
 			}
-			setTurnRightRadians(normalRelativeAngle(turn));						
+			setTurnRightRadians(robocode.util.Utils.normalRelativeAngle(turn));						
 			if(Math.abs(getTurnRemainingRadians()) > 1)
-				setAhead(0);
+			{
+				setFire(1); //setAhead(0);
+			}
 			else
 				setAhead(distance);
 			execute();
 		} while(true);
 	}	
-	private double findRisk(Point2D point)
-	{
 	
+	public double risk(Point2D point)
+	{
+		return 0;	
 	}
 	
 	public void onScannedRobot(ScannedRobotEvent e)
 	{
 		String name = e.getName();
-		Point2D enemyLoc = 
-		enemies.put(name, //put point2D w calculated location of enemy based on distance + bearing/heading 
-		//new Point2D.Double(getX(), getY()); 
-		//write method to generate Point2D obj and return it
+		double enemyX = (location.getX() + Math.sin(getHeadingRadians() + e.getBearingRadians()) * e.getDistance());
+       double enemyY = (location.getY() + Math.cos(getHeadingRadians() + e.getBearingRadians()) * e.getDistance());
+		Point2D enemyLoc = new Point2D.Double(enemyX, enemyY); ;// point2D w calculated location of enemy based on distance + bearing/heading 
+		enemies.put(name, enemyLoc); //note here that put() will replace the previous enemy location if the enemy is already in the hashmap
 	}
 	
 	public void onRobotDeath(RobotDeathEvent e)
@@ -70,47 +73,26 @@ public class MrRobot extends AdvancedRobot
 		enemies.remove(e.getName());
 	}
 	
-	/*
- 	* This helper method uses code from Shiz by Kawigi, which is released under the terms of the KPL.
- 	*/
-	private static Point2D projectPoint(Point2D startPoint, double theta, double dist)
+/*
+ * This helper method uses code from Kawigi, which is released under the terms of the KPL.
+ * http://robowiki.net/wiki/Coriantumr
+ * returns a point dist pixels away from startPoint in the direction of angle.
+ * 
+ */
+	public static Point2D projectPoint(Point2D startPoint, double angle, double dist) 
 	{
-		return new Point2D.Double(startPoint.getX() + dist * Math.sin(theta), startPoint.getY() + dist * Math.cos(theta));
+		return new Point2D.Double(startPoint.getX() + dist * Math.sin(angle), startPoint.getY() + dist * Math.cos(angle));
 	}
-	
-	/*
- 	* This helper method uses code from Shiz by Kawigi, which is released under the terms of the KPL.
- 	*/
-	public static double angle(Point2D point1, Point2D point2)
-	{
-		return Math.atan2(point1.getX()-point2.getX(), point1.getY()-point2.getY());
-	}
-	
- 	public static double absBearing(double x1, double y1, double x2, double y2) 
-	{
-    	double xAbs = x2-x1;
-    	double yAbs = y2-y1;
-    	double hyp = Point2D.distance(x1, y1, x2, y2);
-    	double arcSin = Math.asin(xAbs / hyp);
-    	double bearing = 0;
-    
-  		if (xAbs > 0 && yAbs > 0) 
-  		{ 
-  			bearing = arcSin;
-  		}
-  		else if (xAbs < 0 && yAbs > 0) 
-  		{ 
-  			bearing = MAXRADS + arcSin; 
-  		}
-  		else if (xAbs > 0 && yAbs < 0) 
-  		{ 
-  			bearing = Math.PI - arcSin;
-  		}
-  		else if (xAbs < 0 && yAbs < 0) 
-  		{ 
-  			bearing = Math.PI + arcSin; 
-  		}
-  		return bearing;
-  }
-}
 
+/*
+ * This helper method uses code from Kawigi, which is released under the terms of the KPL.
+ * http://robowiki.net/wiki/Shiz
+ * returns the angle between 2 points in order to move from one to another
+ * uses Math.atan2(x,y) here to correct for possibility of denom being 0 when you calc arctan
+ * 
+ */
+	public static double angle(Point2D point1, Point2D point2) 
+	{
+		return Math.atan2(point1.getX()-point2.getX(), point1.getY()-point2.getY()); 
+	}
+}
