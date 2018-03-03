0d18f0ce89713f955fbb6f81970ef0e403ade5c6
Jay C <35088530+jaykafkachen@users.noreply.github.com>
2018-03-02 23:48:19 -0800

    Update MrRobot.java

--- MrRobot.java
+++ MrRobot.java
@@ -1,13 +1,13 @@
-package TeamMrRobot;
+package FUCK;
 import robocode.*;
 import robocode.util.*;
 import java.util.*;
 import java.awt.geom.*;
 import java.awt.*;
 
-public class MrRobot extends AdvancedRobot
+public class FUCK extends AdvancedRobot
 {
-	static Point2D location, last;
+	static Point2D.Double location, next;
 	static Hashtable<String, Enemy> enemies;
 	static Enemy target;
 	static final double MAXRADS = Math.PI*2;
@@ -17,27 +17,29 @@ public class MrRobot extends AdvancedRobot
 		setTurnRadarRight(Double.POSITIVE_INFINITY);
 		setColors(Color.black, Color.red, Color.red);
 		enemies = new Hashtable<String, Enemy>(); //will store Key String enemy name, Value Enemy obj (enemy location + energy storage)		
-		Point2D next = null;
-
+		target = null;
+		location = new Point2D.Double(getX(), getY());	
 		do
-		{
-			location = new Point2D.Double(getX(), getY());			
-			if(last == null)
-				last = location;
-			boolean changed = false;
+		{			
+			if(next == null)
+				next = location;
 			double moveAngle, distance;
 			moveAngle = distance = 0.0;
+			Rectangle2D.Double area  = new Rectangle2D.Double(40, 40, getBattleFieldWidth()-10, getBattleFieldHeight()-10);
+			Point2D.Double pt = null;
 			do
 			{
-			//	if ()//find the risk of different points, if there's a good new point, then move there
-			//	{
-			//		changed = true;
-			//		next = p; //next equals calculated risk next point
-			//	}
+				if(target!=null)
+				{
+					distance = location.distance(target.getLoc()); //distance to target
+					pt = projectPoint(location, moveAngle, Math.min(distance/2, 300));
+					if (area.contains(pt) && risk(pt)<risk(next))//find the risk of different points, if there's a good new point, then move there
+					{
+						next = pt; //next equals calculated risk next point
+					}
+				}
 				moveAngle += .1;
 			} while(moveAngle < MAXRADS);
-			if (changed)
-				last = location;
 			double turn = angle(next, location)-getHeadingRadians(); //nullPointer here
 			if (Math.cos(turn) < 0)
 			{
@@ -51,32 +53,39 @@ public class MrRobot extends AdvancedRobot
 			}
 			else
 				setAhead(distance);
+			location = new Point2D.Double(getX(), getY());
 			execute();
 		} while(true);
 	}	
 	
-	public double risk(Point2D point)
+	public double risk(Point2D.Double point)
 	{
 		double riskVal = .05*location.distance(point); //greater risk at further away, zero risk when point given is current location
-		double hp = getEnergy();
 		if(hp<50 && (point.getX()<=0 || point.getY()<=0 || point.getX()>=getBattleFieldWidth() || point.getY()>=getBattleFieldHeight()))
 			return 100; //hit a wall, risk is 100% at less than 50 HP because AdvRobots get dmg'd on wall hit
-		Point2D closest = point; //assume the point given is of highest priority
+		Enemy closest = null; 
+		Point2D.Double riskPoint = point;
 		riskVal *= enemies.size(); //higher risk when more enemies on field
-		if(enemies.size()<=2) //if there are few enemies on field/one-on-one, start prioritizing melee
-		{
+		//if(enemies.size()<=2) //if there are few enemies on field/one-on-one, start prioritizing melee
+		//{
 			for (Enemy enemyLoc : enemies.values()) //loop through and find if there is an enemy closer than the point given, will be fast bc 2 or less enemies only
 			{
-    			if(location.distance(closest)>location.distance(enemyLoc.getE()))
-					closest = enemyLoc;
+    			if(location.distance(riskPoint)>location.distance(enemyLoc.getLoc())) //if the last enemy's location is farther than the next enemy
+				{
+					closest = enemyLoc; //set this enemy to be the closest
+					riskPoint = enemyLoc.getLoc();
+				}
 			}
-			if(!closest.equals(point))
-				riskVal -= .05*location.distance(closest); //subtract the distance of the closest robot because we WANT to move there if melee'ing
-		}
-		else
-		{
-		
-		}
+			if(!riskPoint.equals(point)) //if the closest enemy is not the current point projected
+			{
+				riskVal -= .5*location.distance(closest.getLoc()); //subtract the distance of the closest robot because we WANT to move there if melee'ing
+				riskVal = Math.max(riskVal, 10);
+			}	
+		//}
+		//else
+		//{
+			riskVal += riskVal*(1+Math.abs(Math.cos(angle(location, riskPoint) - angle(enemyLoc.getLoc(), location))));
+		//}
 		return riskVal;	
 	}
 	
@@ -84,36 +93,38 @@ public class MrRobot extends AdvancedRobot
 	{
 		String name = e.getName();
 		double enemyX = (location.getX() + Math.sin(getHeadingRadians() + e.getBearingRadians()) * e.getDistance());
-       		double enemyY = (location.getY() + Math.cos(getHeadingRadians() + e.getBearingRadians()) * e.getDistance());
-		Point2D enemyLoc = new Point2D.Double(enemyX, enemyY); ;// point2D w calculated location of enemy based on distance + bearing/heading 
+       	double enemyY = (location.getY() + Math.cos(getHeadingRadians() + e.getBearingRadians()) * e.getDistance());
+		Point2D.Double enemyLoc = new Point2D.Double(enemyX, enemyY); ;// point2D w calculated location of enemy based on distance + bearing/heading 
 		Enemy en = new Enemy(enemyLoc, e.getEnergy());
+		if(target==null || target.getE()>en.getE()) //if we dont have a target or the target is not the weakest/only enemy on the field, target this enemy
+			target = en;
 		enemies.put(name, en); //note here that put() will replace the previous Enemy (location/energy storage object) if the enemy is already in the hashmap
+	}
+	
+	public void onHitByBullet(HitByBulletEvent e)
+	{
+		double dir = getHeadingRadians()+e.getBearingRadians();	
+		next = projectPoint(location, dir+Math.PI/2, 40);
+	}
+	
+	public void onHitWall(HitWallEvent event) 
+	{
+		turnRight(180);	
+		ahead(50);
 	}
 	
 	public void onRobotDeath(RobotDeathEvent e)
 	{
 		enemies.remove(e.getName());
 	}
-	public double fire(double fire)
-	{
-	//we need to decide when we want the robot to shoot, I'll put some filler code in for now
-	//I used the setFire() based on enemy location but we can also use if-else if-else statement or combination of both if needed
-		setFire(300/e.getDistance()); //bullet damage is smaller when the enemy is further away. It gets higher as the enemy gets closer
-					      //any damage number higher than 3 automatically caps at 3 anyway, lowest number caps at 0.1
-		//if(loc<200)
-		//	setFire(0.5);
-		//else if(loc<100)
-		//	setFire(1.5);
-		//else
-		//	setFire(0.1);
-	}
+	
 /*
  * This helper method uses code from Kawigi, which is released under the terms of the KPL.
  * http://robowiki.net/wiki/Coriantumr
  * returns a point dist pixels away from startPoint in the direction of angle.
  * 
  */
-	public static Point2D projectPoint(Point2D startPoint, double angle, double dist) 
+	public static Point2D.Double projectPoint(Point2D.Double startPoint, double angle, double dist) 
 	{
 		return new Point2D.Double(startPoint.getX() + dist * Math.sin(angle), startPoint.getY() + dist * Math.cos(angle));
 	}
@@ -132,10 +143,10 @@ public class MrRobot extends AdvancedRobot
 	
 	public class Enemy //nested class to store enemy energy and location for reference in risk() method
 	{
-		private Point2D loc;
+		private Point2D.Double loc;
 		private double energy;
 		
-		public Enemy(Point2D l, double e)
+		public Enemy(Point2D.Double l, double e)
 		{
 			loc = l;
 			energy = e;
@@ -144,13 +155,13 @@ public class MrRobot extends AdvancedRobot
 		public void setE(double e)
 		{ energy = e;}
 		
-		public void setLoc(Point2D l)
+		public void setLoc(Point2D.Double l)
 		{ loc = l;}
 		
 		public double getE()
 		{ return energy;}
 		
-		public Point2D getLoc()
+		public Point2D.Double getLoc()
 		{ return loc;}
 	}
 }
