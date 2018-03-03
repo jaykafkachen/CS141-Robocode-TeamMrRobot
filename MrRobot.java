17100915bb18ee801bbafc8691c226bd8c7af46b
Jay C <35088530+jaykafkachen@users.noreply.github.com>
2018-03-03 00:26:48 -0800

    Update MrRobot.java

--- MrRobot.java
+++ MrRobot.java
@@ -1,4 +1,4 @@
-package Name;
+package name;
 import robocode.*;
 import robocode.util.*;
 import java.util.*;
@@ -19,12 +19,12 @@ public class Name extends AdvancedRobot
 		enemies = new Hashtable<String, Enemy>(); //will store Key String enemy name, Value Enemy obj (enemy location + energy storage)		
 		target = null;
 		location = new Point2D.Double(getX(), getY());	
+		double moveAngle, distance;
+		moveAngle = distance = 0.0;
 		do
 		{			
 			if(next == null)
 				next = location;
-			double moveAngle, distance;
-			moveAngle = distance = 0.0;
 			Rectangle2D.Double area  = new Rectangle2D.Double(40, 40, getBattleFieldWidth()-10, getBattleFieldHeight()-10);
 			Point2D.Double pt = null;
 			do
@@ -32,27 +32,22 @@ public class Name extends AdvancedRobot
 				if(target!=null)
 				{
 					distance = location.distance(target.getLoc()); //distance to target
-					pt = projectPoint(location, moveAngle, Math.min(distance/2, 300));
+					pt = projectPoint(location, moveAngle, Math.max(distance/2, 300));
 					if (area.contains(pt) && risk(pt)<risk(next))//find the risk of different points, if there's a good new point, then move there
 					{
 						next = pt; //next equals calculated risk next point
 					}
 				}
-				moveAngle += .1;
+				moveAngle += 1;
 			} while(moveAngle < MAXRADS);
-			double turn = angle(next, location)-getHeadingRadians(); //nullPointer here
+			double turn = angle(next, location)-getHeadingRadians(); 
 			if (Math.cos(turn) < 0)
 			{
 				turn += Math.PI;
 				distance = -distance;
 			}
 			setTurnRightRadians(robocode.util.Utils.normalRelativeAngle(turn));						
-			if(Math.abs(getTurnRemainingRadians()) > 1 && getGunHeat()==0) //we will waste a turn if the gun goes off when gun heat is not zero
-			{
-				setFire(1); //setAhead(0);
-			}
-			else
-				setAhead(distance);
+			setAhead(distance);
 			location = new Point2D.Double(getX(), getY());
 			execute();
 		} while(true);
@@ -61,13 +56,11 @@ public class Name extends AdvancedRobot
 	public double risk(Point2D.Double point)
 	{
 		double riskVal = .05*location.distance(point); //greater risk at further away, zero risk when point given is current location
-		if(hp<50 && (point.getX()<=0 || point.getY()<=0 || point.getX()>=getBattleFieldWidth() || point.getY()>=getBattleFieldHeight()))
+		if(getEnergy()<50 && (point.getX()<=0 || point.getY()<=0 || point.getX()>=getBattleFieldWidth() || point.getY()>=getBattleFieldHeight()))
 			return 100; //hit a wall, risk is 100% at less than 50 HP because AdvRobots get dmg'd on wall hit
 		Enemy closest = null; 
 		Point2D.Double riskPoint = point;
 		riskVal *= enemies.size(); //higher risk when more enemies on field
-		//if(enemies.size()<=2) //if there are few enemies on field/one-on-one, start prioritizing melee
-		//{
 			for (Enemy enemyLoc : enemies.values()) //loop through and find if there is an enemy closer than the point given, will be fast bc 2 or less enemies only
 			{
     			if(location.distance(riskPoint)>location.distance(enemyLoc.getLoc())) //if the last enemy's location is farther than the next enemy
@@ -81,11 +74,6 @@ public class Name extends AdvancedRobot
 				riskVal -= .5*location.distance(closest.getLoc()); //subtract the distance of the closest robot because we WANT to move there if melee'ing
 				riskVal = Math.max(riskVal, 10);
 			}	
-		//}
-		//else
-		//{
-			riskVal += riskVal*(1+Math.abs(Math.cos(angle(location, riskPoint) - angle(enemyLoc.getLoc(), location))));
-		//}
 		return riskVal;	
 	}
 	
@@ -99,17 +87,18 @@ public class Name extends AdvancedRobot
 		if(target==null || target.getE()>en.getE()) //if we dont have a target or the target is not the weakest/only enemy on the field, target this enemy
 			target = en;
 		enemies.put(name, en); //note here that put() will replace the previous Enemy (location/energy storage object) if the enemy is already in the hashmap
+ 		setTurnRadarRightRadians(Utils.normalRelativeAngle(turn));
 	}
 	
 	public void onHitByBullet(HitByBulletEvent e)
 	{
 		double dir = getHeadingRadians()+e.getBearingRadians();	
-		next = projectPoint(location, dir+Math.PI/2, 40);
+		next = projectPoint(location, dir+Math.PI/2, 100);
 	}
 	
 	public void onHitWall(HitWallEvent event) 
 	{
-		turnRight(180);	
+		turnRight(Math.PI-getHeadingRadians());
 		ahead(50);
 	}
 	
